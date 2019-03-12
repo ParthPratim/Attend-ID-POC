34e65e8255f94d86c51d7b183615ac9c689ae06e
Parth Pratim Chatterjee <www.ppc007logs@gmail.com>
2019-03-12 21:16:10 +0530

    Add org_id parameter and fix payload decoding in orgs TP

--- src/sawtooth_tf/tf_Organization/processor/payload.py
+++ src/sawtooth_tf/tf_Organization/processor/payload.py
@@ -1,8 +1,14 @@
+import cbor
 from sawtooth_sdk.processor.exceptions import InvalidTransaction
+
 class OrgsPayload:
-    def __init__(self,payload=",,,"):
+    def __init__(self,payload=""):
         try:
-            action,org_name,creator_id = payload.decode().split(',')
+            data = cbor.loads(payload)
+            action = data['action']
+            org_name = data['org_name']
+            creator_id = data['creator_id']
+            org_id = data['org_id']
         except ValueError:
             raise InvalidTransaction("Incorrect Data Serialization")
         if not action:
@@ -11,6 +17,8 @@ class OrgsPayload:
             raise InvalidTransaction("Organization Name not specified")
         if not creator_id:
             raise InvalidTransaction("Organization creator's DigitalID not specified")
+        if not org_id:
+            raise InvalidTransaction("Organization creator's Organizatin ID not specified")
         if action not in ["create"]:
             raise InvalidTransaction("Action submitted is not supported")
         self._action = action
@@ -26,5 +34,6 @@ class OrgsPayload:
     @property
     def creator_id(self):
         return self._creator_id
-
-        
+    @property
+    def org_id(self):
+        return self._org_id
