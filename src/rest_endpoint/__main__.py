b2c6aadc5d42b68577e46e8034b7936a2da67456
Parth Pratim Chatterjee <www.ppc007logs@gmail.com>
2019-05-20 02:02:01 +0530

    Remove src folder copy

--- src/rest_endpoint/__main__.py
+++
@@ -1,4 +0,0 @@
-from rest_endpoint.server.main import server_init
-
-if __name__ == "__main__":
-    server_init()
--- src/rest_endpoint/internals/general.py
+++
@@ -1,7 +0,0 @@
-def hex_to_bytes(hex):
-    bytez = []
-    hex = ''.join( hex.split(" ") )
-    for i in range(0, len(hex), 2):
-        bytez.append( chr( int (hex[i:i+2], 16 ) ) )
-    byte_val = ''.join( bytez )
-    return bytes(byte_val.encode())
